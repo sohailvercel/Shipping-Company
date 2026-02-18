e2d8fb9ec6e506d0840cb14d81b416659fd54388
sohailvercel <sohailvercel@gmail.com>
2026-02-18 21:36:20 +0500

    Update contact.ts

--- server/src/routes/contact.ts
+++ server/src/routes/contact.ts
@@ -95,9 +95,9 @@ router.post("/", async (req, res) => {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
           Accept: "application/json",
-          Origin: req.headers.origin || "",
-          Referer: req.headers.referer || "",
-          "User-Agent": req.headers["user-agent"] || "Mozilla/5.0",
+          // Origin: req.headers.origin || "",
+          // Referer: req.headers.referer || "",
+          // "User-Agent": req.headers["user-agent"] || "Mozilla/5.0",
         },
         timeout: 10000,
       }
